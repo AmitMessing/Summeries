80622448884984d0b82697582051c96611ec3c66
Amit Messing <amitmessing16@gmail.com>
2016-01-03 14:04:08 +0200

    Created run.bat and jsons for the db data

--- run.bat
+++ run.bat
@@ -1,2 +1,13 @@
+cd %~dp0
+
+@echo *** Start mongo db server ***
+start cmd /k "C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe" --dbpath data
+
+"C:\Program Files\MongoDB\Server\3.2\bin\mongoimport" -d summeries -c users --type json --file users.json --jsonArray
+"C:\Program Files\MongoDB\Server\3.2\bin\mongoimport" -d summeries -c media --type json --file media.json --jsonArray
+
+
 start cmd /k node app.js
 start chrome.exe http://localhost:8080/
+
+@PAUSE
